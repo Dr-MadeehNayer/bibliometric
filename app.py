57f03b777c2ca05c463fc4d7d7c199ab8b281775
Dr. Madeeh <m_nayer@hotmail.com>
2024-10-24 21:54:35 +0300

    Update app.py

--- app.py
+++ app.py
@@ -26,7 +26,9 @@ def extract_publication_data(results):
         'title': [],
         'author': [],
         'year': [],
-        'citations': []
+        'citations': [],
+        'abstract': [],  # Added abstract field
+        'url': []        # Added URL field
     }
     
     for pub in results:
@@ -34,6 +36,8 @@ def extract_publication_data(results):
         data['author'].append(pub['bib'].get('author', 'N/A'))
         data['year'].append(pub['bib'].get('pub_year', 'N/A'))
         data['citations'].append(pub.get('num_citations', 0))
+        data['abstract'].append(pub.get('bib', {}).get('abstract', 'N/A'))  # Fetching the abstract
+        data['url'].append(pub.get('pub_url', 'N/A'))  # Fetching the article URL
     
     return pd.DataFrame(data)
 
